1b32df40bcf51f46118e3908ba910706f1213f21
Sajid25 <sajidk25@gmail.com>
2023-11-27 13:37:03 +0600

    modified:   portfolio/portfolio/settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-2t4-5m1z9s2@vtn#esg^ih9_w1v2)zv#-xa26be8g=ee^#1@)&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://nicole-cardenas-portfolio.onrender.com']
+ALLOWED_HOSTS = ['nicole-cardenas-portfolio.onrender.com']
 
 
 # Application definition
